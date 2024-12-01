7bbdc8c8609710df12dc7eec7fbf2fb972b4093c
Moesha1905 <152309855+Moesha1905@users.noreply.github.com>
2024-12-01 04:25:03 +0000

    drag functionality

--- docs/04-js/src/script.js
+++ docs/04-js/src/script.js
@@ -28,3 +28,15 @@ function dragElement(terrariumElement) {
 
 dragElement(document.getElementById('plant1'));
 dragElement(document.getElementById('plant2'));
+dragElement(document.getElementById('plant3'));
+dragElement(document.getElementById('plant4'));
+dragElement(document.getElementById('plant5'));
+dragElement(document.getElementById('plant6'));
+dragElement(document.getElementById('plant7'));
+dragElement(document.getElementById('plant8'));
+dragElement(document.getElementById('plant9'));
+dragElement(document.getElementById('plant10'));
+dragElement(document.getElementById('plant11'));
+dragElement(document.getElementById('plant12'));
+dragElement(document.getElementById('plant13'));
+dragElement(document.getElementById('plant14'));
